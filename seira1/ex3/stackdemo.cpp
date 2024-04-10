858823afa77e183e98bd71cf90203a069c02323b
tzellas <atzellas@gmail.com>
2024-04-10 19:38:46 +0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   seira1/ex2/fullratiotest.cpp
    	modified:   seira1/ex3/stackdemo.cpp

--- seira1/ex3/stackdemo.cpp
+++ seira1/ex3/stackdemo.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-#include <vector>
+
 
 using namespace std;
 
 template <typename T>
-class stack{
-    public:
-    stack(int size)
-    stack(const stack &s)
-    ~stack()
-    const stack & operator = (const stack &s) {
+class stack {
     
+    public:
 
-}
+    stack(int size): size(size) {}
+
+    stack(const stack &s): size(s.size){}
+
+    ~stack(){
+        delete [] data;
+    }
+
+    const stack & operator = (const stack &s) { 
+        size  = y.size
+        return *this;
+    }
+
+
+    
+    private:
+    unsigned size;
+    T *data;
+
+};
